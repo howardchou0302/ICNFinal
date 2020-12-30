1df023051b682531d13aa7d730187c23807ef253
unknown <howardchou0302>
2020-12-31 06:56:33 +0800

    init GameServer

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+            Server.Start(6, 26950);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GameServer/GameServer/Server.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Server
+    {
+        public static int Maxplayers { get; private set; }
+        public static int Port { get; private set; }
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+        private static TcpListener tcpListener;
+
+        public static void Start( int _maxplayers, int _port) 
+        {
+            Maxplayers = _maxplayers;
+            Port = _port;
+
+            Console.WriteLine("Starting Server...");
+            InitializeServerData();
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+            Console.WriteLine($"Server Connected on {Port}");
+        }
+
+        private static void TCPConnectCallback(IAsyncResult _result)
+        {
+            TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+            Console.WriteLine($"Incoming connection from {_client.Client.RemoteEndPoint}...");
+            for(int i = 1; i <= Maxplayers; ++i)
+            {
+                if(clients[i].tcp.socket == null)
+                {
+                    clients[i].tcp.Connect(_client);
+                    return;
+                }
+            }
+            Console.WriteLine($"{_client.Client.RemoteEndPoint} failed to connect: Server fail!");
+        }
+        private static void InitializeServerData()
+        {
+            for(int i = 1; i <= Maxplayers; ++i)
+            {
+                clients.Add(i, new Client(i));
+            }
+        }
+    }
+}
