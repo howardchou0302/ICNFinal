8d923f34c64918cbd3dadc26eb2fd42a0ff88ccf
howardchou0302 <howardchou0302@gmail.com>
2021-01-13 19:42:10 +0800

    script for nonLocal

---
+++ Assets/Player Interaction/BlueAni_NonLocal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueAniNonLocal : MonoBehaviour
+{
+    public Animator anim;
+    public BlueCharacterStats cs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetButtonDown("Horizontal")||Input.GetButtonDown("Vertical")){
+        cs = BlueCharacterStats.Walk;
+      }else if(Input.GetButtonUp("Horizontal")||Input.GetButtonUp("Vertical")){
+        cs = BlueCharacterStats.Idle;
+      }
+      if(cs == BlueCharacterStats.Idle){
+        anim.SetBool("walk",false);
+      }
+      if(cs == BlueCharacterStats.Walk){
+        anim.SetBool("walk",true);
+      }
+    }
+}
